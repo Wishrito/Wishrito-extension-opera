f290c361bc6d190584811d256c6f217ce73da2b2
Wishrito <110767037+Wishrito@users.noreply.github.com>
2024-01-30 17:50:11 +0100

    base files

---
+++ project/content.js
@@ -0,0 +1,12 @@
+// content.js
+
+// Fonction qui sera exécutée lors du chargement de la barre latérale
+function initSidebar() {
+    // Insérer le code pour initialiser la barre latérale ici
+    console.log("Extension de la barre latérale initialisée.");
+}
+
+// Exécuter la fonction d'initialisation une fois la page chargée
+document.addEventListener('DOMContentLoaded', function () {
+    initSidebar();
+});
